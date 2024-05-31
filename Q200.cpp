846c8c48b4ef8ca912a574a16c7ac0ae32fcf918
Sajeev Debnath <sajeevbd04@gmail.com>
2024-05-30 23:30:51 -0400

    Solved Q200

--- Q200.cpp
+++ Q200.cpp
@@ -4,19 +4,45 @@
 #include <vector>
 using namespace std;
 
+void dfs(vector<vector<char>>& grid, int i, int j, int numRows, int numCols) {
+  if (i < 0 || j < 0 || i >= numRows || j >= numCols || grid[i][j] == '0') {
+    return;
+  }
+  grid[i][j] = '0';
+  dfs(grid, i + 1, j, numRows, numCols);
+  dfs(grid, i - 1, j, numRows, numCols);
+  dfs(grid, i, j + 1, numRows, numCols);
+  dfs(grid, i, j - 1, numRows, numCols);
+}
+
 class Solution {
  public:
   int numIslands(vector<vector<char>>& grid) {
+    if (grid.size() == 0) return 0;
     int numIslands = 0;
-    int numRows = grid.size();
-    int numCols = grid[0].size();
-    vector<vector<bool>> visited(numRows, vector<bool>(numCols, false));
-    bool all1svisited = false;
-    int i = 0;
-    int j = 0;
-    while (!all1svisited) {
-      char curr = grid[i][j];
-      // continue here!!
+    int numRows = (grid.size());
+    int numCols = static_cast<int>(grid[0].size());
+
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        if (grid[i][j] == '1') {
+          numIslands++;
+          dfs(grid, i, j, numRows, numCols);
+        }
+      }
     }
+    return numIslands;
   }
 };
+
+int main() {
+  std::vector<std::vector<char>> grid = {
+      {'1', '1', '1', '1', '0'},  // Change string literals to characters
+      {'1', '1', '0', '1', '0'},
+      {'1', '1', '0', '0', '0'},
+      {'0', '0', '0', '0', '0'}};
+  Solution s;
+  cout << s.numIslands(grid) << endl;
+
+  return 0;
+}
--- Q929.cpp
+++ Q929.cpp
@@ -1,128 +1,128 @@
-#include <stdlib.h>
+// #include <stdlib.h>
 
-#include <iostream>
-#include <string>  // Add this line
-#include <unordered_set>
-#include <vector>
+// #include <iostream>
+// #include <string>  // Add this line
+// #include <unordered_set>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-/*********************Attmept 1********************/
+// /*********************Attmept 1********************/
 
+// // class Solution {
+// //  public:
+// //   int numUniqueEmails(vector<string>& emails) {
+// //     // Convert all emails to correct format
+// //     int numEmails = emails.size();
+// //     for (int i = 0; i < numEmails; i++) {
+// //       int sizeString = emails[i].size();
+// //       string curr = emails[i];
+// //       for (int j = 0; j < sizeString; j++) {
+// //         if (curr[j] == '.' || curr[j] == '+') {
+// //           curr[j] = '\0';
+// //         }
+// //       }
+
+// //       for (int j = 0; j < sizeString; j++) {
+// //         if (curr[j] == '\0' && j < sizeString - 1) {
+// //           bool shift = true;
+// //           int k = j;
+// //           if (k + 1 < sizeString) {
+// //             while (shift) {
+// //               if (k + 1 < sizeString) {
+// //                 curr[k] = curr[k + 1];
+// //                 if (k + 1 == sizeString) curr[k + 1] = '\0';
+// //               } else
+// //                 shift = false;
+// //             }
+// //           }
+// //         }
+// //       }
+// //       emails[i] = curr;
+// //     }
+
+// //     // Count unique addresses
+// //     int unique = 0;
+// //     for (int i = 0; i < numEmails; i++) {
+// //       bool duplicate = false;
+// //       string curr = emails[i];
+// //       for (int j = 0; j < numEmails; j++) {
+// //         if (j != i) {
+// //           if (curr == emails[j]) duplicate = true;
+// //         }
+// //       }
+// //       if (!duplicate) unique++;
+// //     }
+// //     return unique;
+// //   }
+// // };
+
+// // Function is too complex so it exceeds time limit
+
+// /*********************End of Attmept 1********************/
+
+// /*********************Attmept 2********************/
 // class Solution {
 //  public:
 //   int numUniqueEmails(vector<string>& emails) {
-//     // Convert all emails to correct format
-//     int numEmails = emails.size();
-//     for (int i = 0; i < numEmails; i++) {
-//       int sizeString = emails[i].size();
+//     int vecSize = emails.size();
+//     unordered_set<string> uniqueEmails;
+
+//     for (int i = 0; i < vecSize; i++) {
 //       string curr = emails[i];
-//       for (int j = 0; j < sizeString; j++) {
-//         if (curr[j] == '.' || curr[j] == '+') {
-//           curr[j] = '\0';
+//       int strSize = curr.size();
+//       string newEmail = "";
+//       int j = 0;
+//       char iterator = curr[0];
+
+//       // Get the name
+//       while (iterator != '@' && iterator != '+') {
+//         if (iterator == '.') {
+//           j++;
+//           iterator = curr[j];
+//         } else {
+//           newEmail += iterator;  // Concatenate the strings
+//           j++;
+//           iterator = curr[j];
 //         }
 //       }
 
-//       for (int j = 0; j < sizeString; j++) {
-//         if (curr[j] == '\0' && j < sizeString - 1) {
-//           bool shift = true;
-//           int k = j;
-//           if (k + 1 < sizeString) {
-//             while (shift) {
-//               if (k + 1 < sizeString) {
-//                 curr[k] = curr[k + 1];
-//                 if (k + 1 == sizeString) curr[k + 1] = '\0';
-//               } else
-//                 shift = false;
-//             }
-//           }
+//       // Get the domain
+//       iterator = curr[j];
+//       bool postAt = false;
+
+//       while (iterator != '\0') {
+//         if (iterator == '@') postAt = true;
+
+//         if (!postAt) {
+//           j++;
+//           iterator = curr[j];
+//         } else {
+//           newEmail += curr[j];
+//           j++;
+//           iterator = curr[j];
 //         }
 //       }
-//       emails[i] = curr;
+
+//       // newEmail is fully formatted
+//       auto search = uniqueEmails.find(newEmail);
+//       if (search == uniqueEmails.end()) {
+//         uniqueEmails.insert(newEmail);
+//       }
 //     }
 
-//     // Count unique addresses
-//     int unique = 0;
-//     for (int i = 0; i < numEmails; i++) {
-//       bool duplicate = false;
-//       string curr = emails[i];
-//       for (int j = 0; j < numEmails; j++) {
-//         if (j != i) {
-//           if (curr == emails[j]) duplicate = true;
-//         }
-//       }
-//       if (!duplicate) unique++;
-//     }
-//     return unique;
+//     return uniqueEmails.size();
 //   }
 // };
 
-// Function is too complex so it exceeds time limit
+// /*********************End of attempt 2*************/
 
-/*********************End of Attmept 1********************/
-
-/*********************Attmept 2********************/
-class Solution {
- public:
-  int numUniqueEmails(vector<string>& emails) {
-    int vecSize = emails.size();
-    unordered_set<string> uniqueEmails;
-
-    for (int i = 0; i < vecSize; i++) {
-      string curr = emails[i];
-      int strSize = curr.size();
-      string newEmail = "";
-      int j = 0;
-      char iterator = curr[0];
-
-      // Get the name
-      while (iterator != '@' && iterator != '+') {
-        if (iterator == '.') {
-          j++;
-          iterator = curr[j];
-        } else {
-          newEmail += iterator;  // Concatenate the strings
-          j++;
-          iterator = curr[j];
-        }
-      }
-
-      // Get the domain
-      iterator = curr[j];
-      bool postAt = false;
-
-      while (iterator != '/0') {
-        if (iterator == '@') postAt = true;
-
-        if (!postAt) {
-          j++;
-          iterator = curr[j];
-        } else {
-          newEmail += curr[j];
-          j++;
-          iterator = curr[j];
-        }
-      }
-
-      // newEmail is fully formatted
-      auto search = uniqueEmails.find(newEmail);
-      if (search == uniqueEmails.end()) {
-        uniqueEmails.insert(newEmail);
-      }
-    }
-
-    return uniqueEmails.size();
-  }
-};
-
-/*********************End of attempt 2*************/
-
-int main() {
-  vector<string> emails = {"test.email+alex@leetcode.com",
-                           "test.e.mail+bob.cathy@leetcode.com",
-                           "testemail+david@lee.tcode.com"};
-  Solution sol;
-  int num = sol.numUniqueEmails(emails);
-  cout << num << endl;
-  return 0;
-}
+// int main() {
+//   vector<string> emails = {"test.email+alex@leetcode.com",
+//                            "test.e.mail+bob.cathy@leetcode.com",
+//                            "testemail+david@lee.tcode.com"};
+//   Solution sol;
+//   int num = sol.numUniqueEmails(emails);
+//   cout << num << endl;
+//   return 0;
+// }
