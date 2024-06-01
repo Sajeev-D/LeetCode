9cd66eb13f0871cb15850132b31b0cb66413cc16
Sajeev Debnath <sajeevbd04@gmail.com>
2024-05-31 22:50:14 -0400

    Solved Q1518

---
+++ Q1518.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int numWaterBottles(int numBottles, int numExchange) {
+    int numDrinks = numBottles;
+    int currBottles = numBottles / numExchange;
+    int remainder = numBottles % numExchange;
+
+    while (currBottles != 0) {
+      numDrinks += currBottles;
+      int oldCurr = currBottles;
+      currBottles = (currBottles + remainder) / numExchange;
+      remainder = (oldCurr + remainder) % numExchange;
+    }
+    return numDrinks;
+  }
+};
+
+int main() {
+  Solution s;
+  cout << s.numWaterBottles(22, 6) << endl;
+  return 0;
+}
