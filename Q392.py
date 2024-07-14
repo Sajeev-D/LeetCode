85ad59e55f47c6dd1d9567945b559de3cf0ef44f
Sajeev Debnath <sajeevbd04@gmail.com>
2024-07-14 17:07:28 -0400

    Solved Q392.

--- Q392.py
+++ Q392.py
@@ -1,20 +1,37 @@
-class Solution(object):
-    def isSubsequence(self, s, t):
-          sSplit = list(s)
-          tSplit = list(t)
-          for i in range (len(s)):
-               if(sSplit[i] in t):
-               else:
-                    return False
-          return True
-               
+# # Attempt 1:
+# class Solution(object):
+#     def isSubsequence(self, s, t):
+#           if(len(s) > len(t)): 
+#                return False
+#           else:
+#                t_index = 0
+#                sSplit = list(s)
+#                for i in range (len(s)):
+#                     if((sSplit[i] not in t) or t.index(sSplit[i]) < t_index):
+#                          return False
+#                     else: 
+#                          t_index = t.find(sSplit[i], t_index)
+#                return True
+                    
 
+# class Solution(object):
+#     def isSubsequence(self, s, t):
+#         if len(s) > len(t):
+#             return False
+        
+#         t_index = 0
+#         for char in s:
+#             t_index = t.find(char, t_index)
+#             if t_index == -1:
+#                 return False
+#             t_index += 1  
+        
+#         return True
 
+# str = "ahbgdc"
+# print(len(str))
 
-str = "ahbgdc"
-print(len(str))
-
-# This is how to convert a string into a character array
-strSplit = list(str)
-print(strSplit)
+# # This is how to convert a string into a character array
+# strSplit = list(str)
+# print(strSplit)
 
